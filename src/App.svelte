5d73d6af25c5b9a6972ead8c673348d44472c78b
Dany Karam <danykaram97@gmail.com>
2021-04-28 22:42:39 -0400

    Footer. Navigation arrows, calculator and graph buttons

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,116 @@
 <script>
-  export let name;
 </script>
 
-<main class="bg-gray-900">
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
-  <div class="py-8 px-4 bg-indigo-100 border border-indigo-500 shadow-lg">
-    Test
+<img class="mx-auto" src="./assets/logo.png" alt="dolar logo" />
+
+<div class="flex flex-col items-center my-12">
+  <div class="text-white text-7xl">$1 = BS. 2.370.023,73</div>
+  <div class="flex justify-end">
+    <p class="text-white text-sm">Actualizado a las 9:00 AM</p>
   </div>
-</main>
+  <div class="flex text-gray-700">
+    <div class="flex flex-col items-center my-12">
+      <div class="flex text-gray-700">
+        <div class="flex h-8 text-xl">
+          <p class="text-white" style="margin-right: 5px;">SEGÚN</p>
+        </div>
+        <div
+          class="h-8 w-8 mr-1 flex justify-center items-center rounded-full bg-gray-200 cursor-pointer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="100%"
+            height="100%"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-chevron-left w-4 h-4"
+          >
+            <polyline points="15 18 9 12 15 6" />
+          </svg>
+        </div>
+
+        <div class="flex h-8 text-xl">
+          <p class="text-primary">Monitor Dolar</p>
+        </div>
+        <div
+          class="h-8 w-8 ml-1 flex justify-center items-center rounded-full bg-gray-200 cursor-pointer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="100%"
+            height="100%"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-chevron-right w-4 h-4"
+          >
+            <polyline points="9 18 15 12 9 6" />
+          </svg>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="flex justify space-between">
+    <div>
+      <button
+        class="rounded-full h-20 w-20 flex items-center justify-center bg-gray-500"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-12 w-12"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9 7h6m0 10v-3m-3 3h.01M9 17h.01M9 14h.01M12 14h.01M15 11h.01M12 11h.01M9 11h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z"
+          />
+        </svg></button
+      >
+    </div>
+    <div>
+      <button
+        class="rounded-full h-20 w-20 flex items-center justify-center bg-gray-500"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-12 w-12"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z"
+          />
+        </svg></button
+      >
+    </div>
+  </div>
+</div>
+<footer
+  class="w-full text-center bg-primary text-white border-t border-grey p-4 absolute bottom-0"
+>
+  © 2021 KURODev.net - Todos los derechos reservados.
+</footer>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+  img {
+    width: 285px;
+    height: 142px;
+    min-width: 275px;
+    min-height: 138px;
   }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      primary: "#14CC60",
+      secondary: "#1D1D1B",
+      white: "##EFF1F3",
+    }),
+    textColor: {
+      primary: "#14CC60",
+      secondary: "#1D1D1B",
+      white: "#EFF1F3",
+    },
   },
   variants: {
     extend: {},
