319c244ff87d518013ce5b710c9bfa1f0d15d026
Dany Karam <danykaram97@gmail.com>
2021-04-30 23:00:40 -0400

    .

--- rollup.config.js
+++ rollup.config.js
@@ -50,8 +50,6 @@ export default {
     // we'll extract any component CSS out into
     // a separate file - better for performance
     css({ output: "bundle.css" }),
-
-    postcss({ extract: "public/build/tailwind.css" }),
     // If you have external dependencies installed from
     // npm, you'll most likely need these plugins. In
     // some cases you'll need additional configuration -
