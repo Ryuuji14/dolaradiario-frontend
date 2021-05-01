d306fd7ec361ff5881dee1e3d949c7887e30f99a
Dany Karam <danykaram97@gmail.com>
2021-05-01 00:00:58 -0400

    pls work 2

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,16 @@
-const production = !process.env.ROLLUP_WATCH;
-const purgecss = require("@fullhuman/postcss-purgecss");
+const tailwindcss = require("tailwindcss");
+
+// only needed if you want to purge
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./src/**/*.svelte", "./public/**/*.html"],
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
 
 module.exports = {
   plugins: [
-    require("postcss-import")(),
-    require("tailwindcss"),
-    production && require("autoprefixer"),
-    production &&
-      purgecss({
-        content: ["./**/*.html", "./**/*.svelte"],
-        defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-      }),
-    production && require("cssnano"),
+    tailwindcss("./tailwind.config.js"),
+
+    // only needed if you want to purge
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
   ],
 };
