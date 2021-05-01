2e312f3e5d09b2de635e7b92a39816d1b3f2ff4d
Gabriel Roa <gaboroab14@gmail.com>
2021-05-01 12:55:06 -0400

    fix

--- src/App.svelte
+++ src/App.svelte
@@ -277,7 +277,7 @@
         {:else}
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="w-6 h-6"
+            class="w-6 h-6 -m-t-8"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
