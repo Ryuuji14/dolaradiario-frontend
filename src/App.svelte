83be4ec2c1c7b9ad1da4163fd3e126d54caa42d6
Dany Karam <danykaram97@gmail.com>
2021-05-03 16:11:51 -0400

    adding duplicate icon

--- src/App.svelte
+++ src/App.svelte
@@ -59,7 +59,7 @@
     updateValues();
   };
   const beautifyNumber = (number) => {
-    return Intl.NumberFormat('es-VE').format(number);
+    return Intl.NumberFormat("es-VE").format(number);
   };
   const parseBeautifulNumber = (number) => {
     return parseFloat(number.toString().replaceAll(".", "").replace(",", "."));
@@ -340,14 +340,14 @@
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-12 w-12"
+            class="w-12 h-12"
             viewBox="0 0 20 20"
             fill="currentColor"
           >
-            <path d="M8 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z" />
             <path
-              d="M6 3a2 2 0 00-2 2v11a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2 3 3 0 01-3 3H9a3 3 0 01-3-3z"
+              d="M7 9a2 2 0 012-2h6a2 2 0 012 2v6a2 2 0 01-2 2H9a2 2 0 01-2-2V9z"
             />
+            <path d="M5 3a2 2 0 00-2 2v6a2 2 0 002 2V5h8a2 2 0 00-2-2H5z" />
           </svg></button
         >
       </div>
@@ -362,7 +362,7 @@
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-12 w-12"
+            class="w-12 h-12"
             viewBox="0 0 20 20"
             fill="currentColor"
           >
@@ -376,7 +376,7 @@
   </div>
 </div>
 <footer
-  class="relative md:fixed bottom-0 flex justify-center w-full p-1 mt-5 text-white border-t md:mt-0 bg-primary"
+  class="relative bottom-0 flex justify-center w-full p-1 mt-5 text-white border-t md:fixed md:mt-0 bg-primary"
 >
   <p>© 2021 KURODev.net</p>
   <p class="hidden md:flex">- Todos los derechos reservados</p>
