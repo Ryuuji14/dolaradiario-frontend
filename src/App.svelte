2122a87897f72552a621c8be552c1ad92d9fdb93
Dany Karam <danykaram97@gmail.com>
2021-05-03 18:54:28 -0400

    changed duplicate icon to outline

--- src/App.svelte
+++ src/App.svelte
@@ -340,14 +340,17 @@
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="w-12 h-12"
-            viewBox="0 0 20 20"
-            fill="currentColor"
+            class="h-12 w-12"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
           >
             <path
-              d="M7 9a2 2 0 012-2h6a2 2 0 012 2v6a2 2 0 01-2 2H9a2 2 0 01-2-2V9z"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
             />
-            <path d="M5 3a2 2 0 00-2 2v6a2 2 0 002 2V5h8a2 2 0 00-2-2H5z" />
           </svg></button
         >
       </div>
